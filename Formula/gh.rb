1fe2557195549dc32c6d257180dede770aec114c
Corey Johnson <probablycorey@gmail.com>
2019-10-29 16:11:15 -0700

    whatever

---
+++ Formula/gh.rb
@@ -0,0 +1,18 @@
+class Gh < Formula
+  desc "GitHub CLI"
+  head "https://github.com/github/gh-cli.git", :branch => "prototype"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "gh", "."
+    bin.install "gh"
+    (bash_completion/"gh.sh").write `#{bin}/gh completion -s bash`
+    (zsh_completion/"_gh").write `#{bin}/gh completion -s zsh`
+  end
+
+  test do
+    help_text = shell_output("#{bin}/gh --help")
+    assert_includes help_text, "Usage:"
+  end
+end
