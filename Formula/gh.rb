f4c4b7a9b016ca3f4005eb22bc08327805c4de4f
Corey Johnson <probablycorey@gmail.com>
2019-11-04 15:24:10 -0800

    Update gh.rb

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -1,11 +1,12 @@
 class Gh < Formula
-  desc "GitHub CLI"
-  head "https://github.com/github/gh-cli.git", :branch => "prototype"
+  desc "wow"
+  homepage "cool"
+  url "https://github.com/probablycorey/homebrew-gghh/releases/download/v0.0.26/darwin_amd64.tar.gz"
+  sha256 "5c9f3862e1bb9ab541ea0a958d4b1391e7b4ea15c0071cb2ac306afc0e8919b2"
 
-  depends_on "go" => :build
+  bottle :unneeded
 
   def install
-    system "go", "build", "-o", "gh", "."
     bin.install "gh"
     (bash_completion/"gh.sh").write `#{bin}/gh completion -s bash`
     (zsh_completion/"_gh").write `#{bin}/gh completion -s zsh`
