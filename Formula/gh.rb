f1705714b215ea9561cd9407c7b88b454cbd7c4e
Corey Johnson <probablycorey@gmail.com>
2019-11-05 16:18:20 -0800

    Update gh.rb

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -1,13 +1,12 @@
 class Gh < Formula
   desc "wow"
   homepage "cool"
-  url "https://github.com/probablycorey/homebrew-gghh/releases/download/v0.0.26/darwin_amd64.tar.gz"
-  sha256 "5c9f3862e1bb9ab541ea0a958d4b1391e7b4ea15c0071cb2ac306afc0e8919b2"
+  url "https://github.com/probablycorey/homebrew-gghh/releases/download/v0.0.51/gghh_0.0.51_darwin_amd64.tar.gz"
 
   bottle :unneeded
 
   def install
-    bin.install "gh"
+    bin.install "gghh"
     (bash_completion/"gh.sh").write `#{bin}/gh completion -s bash`
     (zsh_completion/"_gh").write `#{bin}/gh completion -s zsh`
   end
