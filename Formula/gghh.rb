7e2c55eb6793b0176241c404f7a09da41114a62e
Corey Johnson <probablycorey@github.com>
2019-11-06 14:52:50 -0800

    gghh 0.0.114
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/gghh.rb
+++ Formula/gghh.rb
@@ -1,8 +1,8 @@
 class Gghh < Formula
   desc "wow"
   homepage "cool"
-  url "https://github.com/probablycorey/homebrew-gghh/releases/download/v0.0.65/gghh_0.0.65_darwin_amd64.tar.gz"
-  sha256 "3b7a1e3b73f9e7098f14748c13953fc35976e61ad583bd9904c91308b0abb3bc"
+  url "https://github.com/probablycorey/homebrew-gghh/releases/download/v0.0.114/gh-cli_0.0.114_darwin_amd64.tar.gz"
+  sha256 "8890ac5a60b1cceab4186cb2cf3129b955cb4122ce9c37aa936015139be986b1"
 
   bottle :unneeded
 
